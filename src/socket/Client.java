bd774f5b0065cba0b60e406f6b21ef2dff1376eb
Md Somrat Akbor <somrat876@gmail.com>
2022-12-03 20:30:06 +0600

    server and client both side communication implement

--- src/socket/Client.java
+++ src/socket/Client.java
@@ -1,4 +1,6 @@
 package socket;
+import java.io.DataInput;
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
@@ -11,18 +13,27 @@ public class Client {
      * @throws IOException
      */
     public static void main (String []args) throws IOException{
-        Socket socket = new Socket("localhost", 5243);
-        System.out.println("Connection Successfully Builed");    
-        DataOutputStream output = new DataOutputStream(socket.getOutputStream());
-        Scanner scanner = new Scanner(System.in);
-        String string = "";
-        while(!string.equals("Bye")){
-            string = scanner.nextLine();
-            output.writeUTF(string);
+        try (Socket socket = new Socket("localhost", 5943)) {
+            System.out.println("=========Connection Successfully Builed==========");    
+
+            System.out.println("\n@@@=====Please start your conversation with server=====@@@");    
+            DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+            DataInput input = new DataInputStream(socket.getInputStream());
+            Scanner scanner = new Scanner(System.in);
+            String string = "", serverString = "";
+            while(!string.equals("Bye")){
+                string = scanner.nextLine();
+                output.writeUTF(string);
+                output.flush();
+                serverString = input.readUTF();
+                System.out.println("Server Says: "+serverString);
+            }
+            output.close();
+            socket.close();
+            scanner.close();
+            
         }
-        output.close();
-        socket.close();
-        System.out.println("Connection Closed");
+        System.out.println("!!!!!!!!!--------Connection Closed--------!!!!!!!!!");
 
     }
 }
--- src/socket/Server.java
+++ src/socket/Server.java
@@ -1,24 +1,36 @@
 package socket;
 
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Server {
     public static void main(String []args) throws IOException{
-        ServerSocket serverSocket = new ServerSocket(5243);
-        System.out.println("Server is wating for client");
+        ServerSocket serverSocket = new ServerSocket(5943);
+        System.out.println("=========Server is wating for client=========");
         Socket socket = serverSocket.accept();
-        System.out.println("Client request is accepted");
+        System.out.println("@@@=====Client request is accepted=====@@@");
+        System.out.println("###=====Wating for Client Message=====###");
         DataInputStream inputStream = new DataInputStream(socket.getInputStream());
-        String  string = "";
+
+        DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
+       try (Scanner scanner = new Scanner(System.in)) {
+        String  string = "", clientString = "";
         while(!string.equals("Bye")){
             string = inputStream.readUTF();
             System.out.println("Client says: "+string);
-        } 
+            clientString = scanner.nextLine();
+            outputStream.writeUTF(clientString);
+            outputStream.flush();
+        }
+    }
         inputStream.close();
         serverSocket.close();
-
+        outputStream.close();
+        socket.close();
+        System.out.println("!!!!!!!!!--------Connection Closed--------!!!!!!!!!");
     }
 }
